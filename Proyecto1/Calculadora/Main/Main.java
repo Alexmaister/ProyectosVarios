d8f70b851d9666058600d4b98ff37494baee809c
LuisZ1 <luiszumarraga95@gmail.com>
2016-10-06 22:36:15 +0200

    Modificado pseudocodigo Main

--- Proyecto1/Calculadora/Main/Main.java
+++ Proyecto1/Calculadora/Main/Main.java
@@ -13,56 +13,85 @@ import MenusYValidaciones.*;
  * */
 
 /*
- * ANALISIS:
- *     * Programa que simulara una calculadora con operaciones tanto aritmeticas como trigonometricas
- * 
- * Requisitos:
- *     * Calcular suma
- *     * Calcular resta
- *     * Calcular multiplicacion
- *     * Calcular division
- * 
- * Entradas:
- *     * Opciones de  interacciOn con el menu
- *     * Numeros con los que se realizaran las operaciones
- * 
- * Salidas:
- *     * Se mostraran por pantalla el resultado de las operaciones que se realizen
- *   
- * Restrincciones: 
- *     * Ninguna
- *     
- * Suposiciones: 
- *     * Ninguna
- * 
- * 
- * PSEUDOCODIGO GENERALIZADO:
- * 
- * INICIO
- * 
- * 	REPETIR
- * 
- * 		MOSTRAR Y VALIDAR MENU PRINCIPAL
- * 
- * 		SI	OPCION ES DISTINTO A SALIR
- * 
- * 			SEGUN OPCION
- * 
- * 				CASO 1:
- * 
- * 					OPERACIONES ARITMETICAS
- * 
- * 				CASO 2:
- * 
- * 					OPRACIONES TRIGONOMETRICAS
- * 
- * 			FIN SEGUN
- * 
- * 		FIN SI
- * 
- * 	MIENTRAS OPCION NO SEA SALIR
- * 
- * FIN
+  ANALISIS:
+     * Programa que simulara una calculadora con operaciones tanto aritmeticas como trigonometricas
+  
+  Requisitos:
+      * Calcular suma
+      * Calcular resta
+      * Calcular multiplicacion
+      * Calcular division
+  
+  Entradas:
+      * Opciones de  interacciOn con el menu
+      * Numeros con los que se realizaran las operaciones
+  
+  Salidas:
+      * Se mostraran por pantalla el resultado de las operaciones que se realizen
+    
+  Restrincciones: 
+      * Ninguna
+      
+  Suposiciones: 
+      * Ninguna
+  
+  
+ PSEUDOCODIGO GENERALIZADO:
+  
+  INICIO
+  
+  	REPETIR
+  
+  		MOSTRAR Y VALIDAR MENU PRINCIPAL
+  
+  		SI	OPCION ES DISTINTO A SALIR
+  
+ 			SEGUN OPCION
+  
+  				CASO 1:	OPERACIONES ARITMETICAS
+  				
+  					SI	OPCION ES DISTINTO A SALIR
+  					
+  						SEGUN OPCION
+ 							
+ 							CASO 0: SALIR
+  							CASO 1: SUMAR
+  							CASO 2: RESTAR
+  							CASO 3: MULTIPLICAR					
+  							CASO 4: DIVIDIR
+  							CASO 5: RAIZ
+  							CASO 6: POTENCIA
+  							CASO 7: SUMATORIO
+  							
+						FIN SEGUN
+					
+  					FIN SI
+  					
+  				CASO 2: OPERACIONES TRIGONOMETRICAS
+  				
+  					SI	OPCION ES DISTINTO A SALIR
+  					
+  						SEGUN OPCION
+ 							
+ 							CASO 0: SALIR
+  							CASO 1: SENO
+  							CASO 2: COSENO
+  							CASO 3: TANGENTE					
+  							CASO 4: SECANTE
+  							CASO 5: COSECANTE
+  							CASO 6: COTANGENTE
+  							
+						FIN SEGUN
+					
+  					FIN SI
+  
+  			FIN SEGUN
+  
+  		FIN SI
+  
+  	MIENTRAS OPCION NO SEA SALIR
+  
+  FIN
  * */
 public class Main {
 
