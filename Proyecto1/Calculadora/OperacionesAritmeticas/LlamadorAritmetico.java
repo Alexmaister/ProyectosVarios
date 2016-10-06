f77e213942c57cda013e74faadbf7884e51a0254
LuisZ1 <luiszumarraga95@gmail.com>
2016-10-06 23:14:57 +0200

    Modificado Pseudocodigo de los llamadores
    Hay que revisarlo

--- Proyecto1/Calculadora/OperacionesAritmeticas/LlamadorAritmetico.java
+++ Proyecto1/Calculadora/OperacionesAritmeticas/LlamadorAritmetico.java
@@ -24,8 +24,18 @@ FIN SI
  */
 /*
   
+  DESCRIPCION: el subprograma pedirá la operacion que se quiere realizar del menu
+  				 cuantos operandos vamos a usar,
+   				 y los operandos con los que se quiere operar 
+   				 
+  PRECONDICIONES: La opcion del submenu debe estar entre 0 y 7
+  ENTRADAS: Opcion de menu y los operandos
+  SALIDAS: -
+  POSTCONDICIONES: 
+  ENTRADA/SALIDA:
  
  */
+
 public class LlamadorAritmetico {
 	
 	public static void llamadorAritmetico(){
--- Proyecto1/Calculadora/OperacionesTrigonometricas/LlamadorTrigonometrico.java
+++ Proyecto1/Calculadora/OperacionesTrigonometricas/LlamadorTrigonometrico.java
@@ -18,6 +18,19 @@ FIN SEGUN
 
 FIN SI
 */
+/*
+
+ DESCRIPCION: el subprograma pedirá la operacion que se quiere realizar del menu
+  				 cuantos operandos vamos a usar,
+   				 y los operandos con los que se quiere operar 
+   				 
+  PRECONDICIONES: La opcion del submenu debe estar entre 0 y 6
+  ENTRADAS: Opcion de menu y los operandos
+  SALIDAS: -
+  POSTCONDICIONES: 
+  ENTRADA/SALIDA:
+
+*/
 public class LlamadorTrigonometrico {
 	
 	public static void llamadorTrigonometrico(){
