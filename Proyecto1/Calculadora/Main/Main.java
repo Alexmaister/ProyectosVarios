886a1d0229056b76c57762d4b6e0884d28a72e7b
Alexmaister <aortizurita@gmail.com>
2016-10-06 12:02:40 +0200

    Arreglo en analisis, pseudicodigo borrado

--- Proyecto1/Calculadora/Main/Main.java
+++ Proyecto1/Calculadora/Main/Main.java
@@ -28,13 +28,7 @@ import Validaciones.*;
  * 
  * Salidas:
  *     * Se mostraran por pantalla el resultado de las operaciones que se realizen
- * 
- * Precondiciones:
- *     * Los datos introducidos deben ser numeros enteros o reales
- * 
- * Postcondiciones:
- *     * El resultado sera un numero real
- *    
+ *   
  * Restrincciones: 
  *     * Ninguna
  *     
@@ -43,119 +37,6 @@ import Validaciones.*;
  * 
  * 
  * PSEUDOCODIGO GENERALIZADO:
- * 
- * INICIO
- * 	  HACER
- *        MOSTRAR MENU PRINCIPAL
- * 		  VALIDAR OPCION
- *    SI OPCION NO ES SALIR
- * 		
- *    SI OPCION ES 1	
- * 	      MOSTRAR MENU ARITMETICA
- * 		  VALIDAR OPCION 2
- * 
- * 		  SEGUN OPCION
- *	          OPCION 1
- * 			  	  HACER
- * 				      SUMAR
- * 				      MOSTRAR RESULTADO
- * 					  VALIDAR SEGUIR
- * 				  MIENTRAS SEGUIR SEA SI
- * 
- * 			  OPCION 2
- * 				  HACER
- * 				      RESTAR
- * 					      MOSTRAR RESULTADO
- * 						  VALIDAR SEGUIR
- * 				  MIENTRAS SEGUIR SEA SI
- *  
- * 			  OPCION 3
- * 				  HACER
- * 				      MULTIPLICAR
- * 					  MOSTRAR RESULTADO
- * 					  VALIDAR SEGUIR 
- * 				  MIENTRAS SEGUIR SEA SI
- * 
- * 			  OPCION 4
- * 				  HACER
- * 					  DIVIDIR
- * 					  MOSTRAR RESULTADO
- * 				      VALIDAR SEGUIR 
- * 			      MIENTRAS SEGUIR SEA SI
- *  
- * 			  OPCION 5
- * 				  HACER
- * 			 	 	  RAIZ
- * 					  MOSTRAR RESULTADO
- * 					  VALIDAR SEGUIR
- * 				  MIENTRAS SEGUIR SEA SI
- *  
- * 			  OPCION 6
- * 				  HACER
- * 				      POTENCIA
- * 					  MOSTRAR RESULTADO
- * 				      VALIDAR SEGUIR
- * 				  MIENTRAS SEGUIR SEA SI
- *   
- * 			  OPCION 7
- * 				  HACER
- * 				      SUMATORIO
- * 				      MOSTRAR RESULTADO
- * 				  	  VALIDAR SEGUIR
- * 				  MIENTRAS SEGUIR SEA SI
- *  		
- * 			SINO SI OPCION ES 2
- * 			
- * 				MOSTRAR MENU TRIGONOMETRIA
- * 				VALIDAR OPCION 2
- * 
- * 					OPCION 1
- * 						HACER
- * 						 	SENO
- * 						 	MOSTRAR RESULTADO
- * 							VALIDAR SEGUIR
- * 						MIENTRAS SEGUIR SEA SI
- * 
- * 					OPCION 2
- * 						HACER
- * 						 	COSENO
- * 						 	MOSTRAR RESULTADO
- * 							VALIDAR SEGUIR 
- * 						MIENTRAS SEGUIR SEA SI
- *  
- * 					OPCION 3	
- * 						HACER
- * 						 	TANGENTE
- * 						 	MOSTRAR RESULTADO
- * 							VALIDAR SEGUIR
- * 						MIENTRAS SEGUIR SEA SI
- *  
- * 					OPCION 4
- * 						HACER
- * 						 	SECANTE
- * 						 	MOSTRAR RESULTADO
- * 							VALIDAR SEGUIR
- * 						MIENTRAS SEGUIR SEA SI
- * 
- * 					OPCION 5
- * 						HACER
- * 						 	COSECANTE
- * 						 	MOSTRAR RESULTADO
- * 							VALIDAR SEGUIR
- * 						MIENTRAS SEGUIR SEA SI
- * 
- * 					OPCION 6
- * 						HACER
- * 						 	COTANGENTE
- * 						 	MOSTRAR RESULTADO
- * 							VALIDAR SEGUIR
- * 						MIENTRAS SEGUIR SEA SI
- * 			FIN SINO
- * 		FIN SI
- * 
- * 	MIENTRAS OPCION NO SEA SALIR Y OPCION 2 SEA VOLVER
- * 
- * FIN
  * 
  * */
 public class Main {
