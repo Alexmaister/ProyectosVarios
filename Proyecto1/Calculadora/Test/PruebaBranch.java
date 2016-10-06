72787ad926a9109d5e0fdd98204eaded9eb68847
Alexmaister <aortizurita@gmail.com>
2016-10-06 23:38:26 +0200

    prueba branch segunda

--- Proyecto1/Calculadora/Test/PruebaBranch.java
+++ Proyecto1/Calculadora/Test/PruebaBranch.java
@@ -6,6 +6,8 @@ public class PruebaBranch {
 	public static void main(String[] args) {
 		
 		System.out.print("Este es mio jajaja Ale");
+		System.out.print("Este es mio jajaja Ale");
+		System.out.print("Este es mio jajaja Ale");
 	}
 
 }
