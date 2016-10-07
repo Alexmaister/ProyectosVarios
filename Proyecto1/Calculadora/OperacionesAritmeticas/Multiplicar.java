32521d183c76e6a7119e3da5cb72922f113d2893
LuisZ1 <luiszumarraga95@gmail.com>
2016-10-07 08:53:23 +0200

    Añadido pseudocodigo a la multiplicacion

--- Proyecto1/Calculadora/OperacionesAritmeticas/Multiplicar.java
+++ Proyecto1/Calculadora/OperacionesAritmeticas/Multiplicar.java
@@ -1,4 +1,16 @@
 package OperacionesAritmeticas;
+/*
+  
+ CABECERA: public int multiplicar (int numeroOperandos, int operando1, int operando2)
+ 
+ COMENTARIO: El programa multiplicará los operandos recibidos
+ PRECONDICIONES: 
+ ENTRADAS: numero de radicandos con los que va a operar y los operandos
+ SALIDAS: el resultado de la operacion
+ ENTRADA/SALIDA:
+ POSTCONDICIONES: el resultado se envia a un subprograma que lo imprima en pantalla
+
+ */
 
 public class Multiplicar {
 
