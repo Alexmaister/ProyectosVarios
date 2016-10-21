9ecff5afb42bc41293c035747a3abbd0d50458a1
Alexmaister <aortizurita@gmail.com>
2016-10-21 09:27:56 +0200

    Luis no escribas modulos que no son tuyos

--- Proyecto1/Calculadora/OperacionesAritmeticas/Sumar.java
+++ Proyecto1/Calculadora/OperacionesAritmeticas/Sumar.java
@@ -1,25 +1,6 @@
 package OperacionesAritmeticas;
 
-/*
-  CABECERA: 
- 
- COMENTARIO: 
- PRECONDICIONES: 
- ENTRADAS: 
- SALIDAS: 
- ENTRADA/SALIDA:
- POSTCONDICIONES: 
- */
-
 public class Sumar {
 	
-public static double suma (double sumando1, double sumando2){
-		
-		double resultado = 0.0;
-		
-		resultado = sumando1 + sumando2;
-		
-		return resultado;
-	}
 
 }
--- Proyecto1/Calculadora/OperacionesAritmeticas/Sumatorio.java
+++ Proyecto1/Calculadora/OperacionesAritmeticas/Sumatorio.java
@@ -1,24 +1,7 @@
 package OperacionesAritmeticas;
 
-/*
-CABECERA: 
 
-COMENTARIO: 
-PRECONDICIONES: 
-ENTRADAS: 
-SALIDAS: 
-ENTRADA/SALIDA:
-POSTCONDICIONES: 
-*/
 
 public class Sumatorio {
-	
-	public static int sumatorio (int numeroSumatorio){
-		int resultado=0;
-		
-		System.out.println("El numero al que va a calcularle el sumatorio es: "+numeroSumatorio);
-		
-		return resultado;		
-	}
 
 }
