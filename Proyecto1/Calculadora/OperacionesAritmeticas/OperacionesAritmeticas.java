684daf29349c902ac0e44a812150976eb2a487b4
Paulo Soares <pauloxti@gmail.com>
2016-12-06 20:12:06 +0100

    ttest

--- Proyecto1/Calculadora/OperacionesAritmeticas/OperacionesAritmeticas.java
+++ Proyecto1/Calculadora/OperacionesAritmeticas/OperacionesAritmeticas.java
@@ -21,14 +21,14 @@ public class OperacionesAritmeticas
 
 
 /*
-CABECERA: public int multiplicar (int numeroOperandos, int operando1, int operando2)
-
-COMENTARIO: El programa multiplicará los operandos recibidos.
-PRECONDICIONES: 
-ENTRADAS: numero de operandos con los que va a operar
-SALIDAS: el resultado de la operacion
-ENTRADA/SALIDA:
-POSTCONDICIONES: el resultado se envia al main, donde un subprograma que lo imprimirá en pantalla
+	CABECERA: public int multiplicar (int numeroOperandos, int operando1, int operando2)
+	
+	COMENTARIO: El programa multiplicará los operandos recibidos.
+	PRECONDICIONES: 
+	ENTRADAS: numero de operandos con los que va a operar
+	SALIDAS: el resultado de la operacion
+	ENTRADA/SALIDA:
+	POSTCONDICIONES: el resultado se envia al main, donde un subprograma que lo imprimirá en pantalla
 
 */
 
