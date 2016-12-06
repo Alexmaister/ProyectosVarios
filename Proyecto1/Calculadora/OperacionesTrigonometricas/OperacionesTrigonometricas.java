6c2abf196313ad481ca278dc5f2e48f2eb386d0c
Alexmaister <aortizurita@gmail.com>
2016-12-06 22:07:53 +0100

    terminado opraciones falta interfaz

--- Proyecto1/Calculadora/OperacionesTrigonometricas/OperacionesTrigonometricas.java
+++ Proyecto1/Calculadora/OperacionesTrigonometricas/OperacionesTrigonometricas.java
@@ -3,6 +3,8 @@ import java.math.*;
 
 public class OperacionesTrigonometricas 
 {
+	
+	double PI =3.1416;
 	/*CABECERA: double coseno(double numero)
 	 * DESCRIPCION:
 	 * ENTRADAS:
@@ -12,7 +14,7 @@ public class OperacionesTrigonometricas
 	 */
 	public static double coseno(double numero){
 		double resultado=0;
-		
+		numero=convertirRadianes(numero);
 		resultado=Math.cos(numero);
 		
 		return resultado;
@@ -31,8 +33,8 @@ public class OperacionesTrigonometricas
 	public static double seno(double numero){
 		
 		double resultado=0;
-		
-			resultado=Math.sin(numero);
+		numero=convertirRadianes(numero);
+		resultado=Math.sin(numero);
 		return resultado;
 	} 
 	
@@ -47,7 +49,7 @@ public class OperacionesTrigonometricas
 	public static double tangente(double numero){
 		//
 		double resultado=0;
-		
+		numero=convertirRadianes(numero);
 		resultado=Math.tan(numero);
 		
 		return resultado;
@@ -64,7 +66,7 @@ public class OperacionesTrigonometricas
 	public static double cosecante(double numero){
 		
 		double resultado=0;
-		
+		numero=convertirRadianes(numero);
 		resultado=Math.acos(numero);
 		
 		return resultado;
@@ -78,6 +80,15 @@ public class OperacionesTrigonometricas
 	 * POSTCONDICIONES:
 	 */
 	
+	public static double secante (double numero){
+		
+		double resultado=0;
+		numero=convertirRadianes(numero);
+		resultado=Math.asin(numero);
+		
+		return resultado;
+	}
+	
 	/*CABECERA: double cotangente(double numero)
 	 * DESCRIPCION:
 	 * ENTRADAS:
@@ -85,4 +96,36 @@ public class OperacionesTrigonometricas
 	 * SALIDAS:
 	 * POSTCONDICIONES:
 	 */
+	
+	public static double cotangente (double numero){
+		
+		double resultado=0;
+		numero=convertirRadianes(numero);
+		resultado=Math.atan(numero);
+		
+		return resultado;
+	}
+	
+	/*CABECERA: double convertirRadianes(double numero)
+	 * DESCRIPCION:Funcion que cogera un numero entre 0 y 360 de entrada y lo convertira en radianes
+	 * ENTRADAS:Numero real
+	 * PRECONDICIONES:Ninguna
+	 * SALIDAS:Numero en radianes
+	 * POSTCONDICIONES:El resultado sera un numero en radianes,
+	 * 					devolvera -1 en caso de que el numero de entrada no este en el rango permitido,
+	 *  				se devolvera asociado al nombre
+	 */
+	
+	public static double convertirRadianes(double numero){
+		
+		final double PI =3.1416;
+		double resultado=0;
+		
+		if(numero>=0 && numero<=360){
+			
+			resultado=(numero*PI)/180;
+		}
+		return resultado;
+	}
+	
 }
---
+++ Proyecto1/Calculadora/Test/PruebaTrigonometrica.java
@@ -0,0 +1,15 @@
+package Test;
+import OperacionesTrigonometricas.*;
+public class PruebaTrigonometrica {
+	
+	public static void main(String[]args){
+		
+		double numero=90;
+		double resultado=0;
+		
+		resultado=OperacionesTrigonometricas.seno(numero);
+		System.out.println(resultado);
+		
+	}
+
+}
